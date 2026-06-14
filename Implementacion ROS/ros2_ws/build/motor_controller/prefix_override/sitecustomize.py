a66c6a9b4f46f27821d4b65786daa7515702cef4
Caspian258 <A01540625@tec.mx>
2026-06-14 14:08:18 -0600

    Limpiar repo: eliminar proyecto ROS2 y untrackear archivos auto-generados
    - Eliminar Implementacion ROS/ (proyecto ROS2 anterior, proyecto distinto)
    - Eliminar Validaciones numericas/ (scripts de validación movidos)
    - Untrackear .vscode/ (auto-generado por PlatformIO, ya está en .gitignore)
    - Agregar .vscode/ a .gitignore para excluirlo en el futuro

--- Implementacion ROS/ros2_ws/build/motor_controller/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/bassdrumm/Documents/PlatformIO/Projects/motor_pd_controller/ros2_ws/install/motor_controller'
--- Implementacion ROS/ros2_ws/build/motor_controller/share/motor_controller/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/bassdrumm/Documents/PlatformIO/Projects/motor_pd_controller/ros2_ws/build/motor_controller"
